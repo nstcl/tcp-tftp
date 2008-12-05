2a5625e7d4519d3151706810ee45e0eb68c3b918
Amir.Schwimmer <Amir.Schwimmer@e33953e4-ae81-11dd-8aed-b72f48e3f726>
2008-12-05 06:28:03 +0000

    updated dotnet client

--- tcpTftpClientDotNet/Program.cs
+++ tcpTftpClientDotNet/Program.cs
@@ -12,13 +12,15 @@ namespace tcpTftpClientDotNet
             //byte[] rxBuf = new byte[516];
             //int rxCount;
             Console.WriteLine("Starting Client");
-            Client c = new Client("10.0.0.5", 5069);
+            Client c = new Client("192.168.20.66", 5069);
             //c.sendTftpPacket(Client.OP_CODE.RRQ, "./server.log", null,-1);
             //rxCount = c.recvTftpPacket(ref rxBuf);
             
-            //Console.WriteLine(c.GetFile("./1.txt"));
-            c.PutFile("tcpTftpClientDotNet.vshost.exe.manifest");
-
+            
+            Console.WriteLine("Sending File");
+            c.PutBinaryFile("..\\..\\Client.cs");
+            Console.WriteLine("Receiving File");
+            Console.WriteLine(c.GetFile("Client.cs"));
             //Console.WriteLine("Press Any Key To Exit ...");
             //Console.ReadKey(true);
         }
